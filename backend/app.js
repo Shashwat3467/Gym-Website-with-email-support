a0d38b5e4d9f18854e46ab0d2bb2e08d27147424
Shashwat Kadam <shashwatkadam11@gmail.com>
2026-02-18 18:22:17 +0530

    Final changes done

--- backend/app.js
+++ backend/app.js
@@ -56,5 +56,5 @@ router.post("/send/mail", async (req, res) => {
 app.use("/api", router);
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server listening at port ${process.env.PORT}`);
+  console.log(`Server listening to port ${process.env.PORT}`);
 });
